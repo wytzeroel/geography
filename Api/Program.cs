2dbbca275ee0f01dae4bd685d501069184830a73
wroel <wytze.roel@gmail.com>
2024-10-06 20:16:52 +0200

    Add blazor

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
     })
